4c0b62a56f5ff9a057ec6bbd30657289f99860f4
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-15 16:12:32 +0300

    fix jf
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

--- housekeeping/deploy.sh
+++ housekeeping/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 sqd auth -k $SUBSQUID_KEY
 
@@ -10,27 +11,32 @@ if [ "$dbmigrate" = true ]; then
     npx squid-typeorm-migration create
     npx squid-typeorm-migration apply
     npm run build
+    printf "✅ database was migrated! \n"
 fi
 
 # remove squid
 if [ "$fullClean" = true ]; then
     sqd kill $project -f
+    printf "♻️ $project was deleted! \n"
 fi
 
 # deploy squid
 if [ "$deploy" = true ] || [ "$fullClean" = true ]; then
     sqd init $project --template .
     sqd deploy $project --update --no-stream-logs
+    printf "✅👌 $project was deployed! \n"
 fi
 
 # restart squid
-if [ "$restart" = true ];
+if [ "$restart" = true ]; then
     sqd redeploy $project@v1
+    printf "✅ $project was restarted! \n"
 fi
 
 # hardreset squid
 if [ "$hardreset" = true ]; then
     sqd init $project --template .
     sqd deploy $project -u -r --no-stream-logs
+    printf "✅ hardreset of $project is done! \n"
 fi
 
