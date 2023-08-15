7ea103d248a903b784eee6089a6d1c9ea4613f97
Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>
2023-08-15 15:42:21 +0300

    test build
    Signed-off-by: Nikita Zaporozhets <zaporozhets@soramitsu.co.jp>

---
+++ housekeeping/deploy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+# database migration
+if [ "$dbmigrate" = true ]; then
+    npm ci
+    make up
+    npx squid-typeorm-migration generate
+    npx squid-typeorm-migration create
+    npx squid-typeorm-migration apply
+    npm run build
+fi
+
+# remove squid
+if [ "$fullClean" = true ]; then
+    sqd kill $project -f
+fi
+
+# deploy squid
+if [ "$deploy" = true ] || [ "$fullClean" = true ]; then
+    sqd init $project --template .
+    sqd deploy $project --update --no-stream-logs
+fi
+
+# restart squid
+if [ "$restart" = true ];
+    sqd redeploy $project@v1
+fi
+
+# hardreset squid
+if [ "$hardreset" = true ]; then
+    sqd init $project --template .
+    sqd deploy $project -u -r --no-stream-logs
+fi
+
