0980cb6fd498574231b49574d77b1ca651aeef45
sorabot <admin@soramitsu.co.jp>
2023-09-08 09:07:42 +0000

    automigrate
    Signed-off-by: sorabot <admin@soramitsu.co.jp>

--- db/migrations/1694164044562-Data.js
+++ db/migrations/1694164044562-Data.js
@@ -1,5 +1,5 @@
-module.exports = class Data1676633197355 {
-    name = 'Data1676633197355'
+module.exports = class Data1694164044562 {
+    name = 'Data1694164044562'
 
     async up(db) {
         await db.query(`CREATE TABLE "history_element" ("id" character varying NOT NULL, "block_number" integer NOT NULL, "extrinsic_idx" text, "extrinsic_hash" text, "timestamp" numeric NOT NULL, "address" text NOT NULL, "reward" jsonb, "extrinsic" jsonb, "transfer" jsonb, CONSTRAINT "PK_b10b09ee684b794e1ca6dc2470c" PRIMARY KEY ("id"))`)
---
+++ db/migrations/1694164045341-Data.js
@@ -0,0 +1,11 @@
+module.exports = class Data1694164045341 {
+    name = 'Data1694164045341'
+
+    async up(db) {
+
+    }
+
+    async down(db) {
+
+    }
+}
