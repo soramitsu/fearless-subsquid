a30bfd383cbffdddda7927caaa1cfe7268dd1db8
anonymousvoxx <97196175+anonymousvoxx@users.noreply.github.com>
2023-12-27 00:56:30 +0300

    added transfer allow death processing

---
+++ src/mappings/balances/calls/transfer_allow_death.ts
@@ -0,0 +1,44 @@
+import { UnknownVersionError } from '../../../common/errors'
+import { encodeId, getOriginAccountId, isAdressSS58 } from '../../../common/tools'
+import { BalancesTransferAllowDeathCall } from '../../../types/generated/calls'
+import { CallContext, CallHandlerContext } from '../../types/contexts'
+import { saveTransfer } from '../../util/entities'
+
+interface EventData {
+    to: Uint8Array
+    amount: bigint
+}
+
+function getCallData(ctx: CallContext): EventData | undefined {
+    const call = new BalancesTransferAllowDeathCall(ctx)
+    if (call.isV9420) {
+        const { dest, value } = call.asV9420
+        return {
+            to: dest as Uint8Array,
+            amount: value,
+        }
+    } else {
+        throw new UnknownVersionError(call.constructor.name)
+    }
+}
+
+export async function handleTransferAllowDeath(ctx: CallHandlerContext) {
+    const data = getCallData(ctx)
+    if (!data) return
+
+    const accountId = getOriginAccountId(ctx.call.origin)
+    if (!accountId) return
+
+    await saveTransfer(ctx, {
+        id: ctx.call.id,
+        extrinsicHash: ctx.extrinsic.hash,
+        timestamp: ctx.block.timestamp,
+        blockNumber: ctx.block.height,
+        fromId: accountId,
+        toId: isAdressSS58(data.to) ? encodeId(data.to) : null,
+        amount: data.amount,
+        success: ctx.call.success,
+        extrinsicIdx: ctx.extrinsic.id,
+        fee: ctx.extrinsic.fee,
+    })
+}
